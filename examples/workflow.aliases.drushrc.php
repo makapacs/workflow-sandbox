4723dc82fd556ff27bdaf9a56ecf746cb6f51b3b
Margots <makapacs@hotmail.com>
2014-10-18 15:53:23 -0500

    add sample alias

---
+++ examples/workflow.aliases.drushrc.php
@@ -0,0 +1,9 @@
+<?php
+$aliases['workflow'] = array (
+    'uri' => 'workflow',
+    'root' => '/home/websites/workflow',
+    'db-user' => 'workflow_user',
+    'db-pwd' => 'password1',
+    'db-name' => 'workflow_db',
+);
+?>
